19c818bf51922a32ef3eacc257bf955c817374d9
DINOREX <225062012+amiranjum113@users.noreply.github.com>
2026-02-10 18:18:32 -0700

    Initial commit

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -49,8 +49,7 @@ begin
        w_addends <= x"FF"; w_Cin <= '1'; wait for 10 ns;
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
-	
-		wait; -- wait forever
+	   wait; -- wait forever
 	end process;	
 	-----------------------------------------------------	
 	
